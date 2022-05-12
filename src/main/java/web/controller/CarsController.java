67dbaa7b16cdbb4088e1fb9baa74955c321a8dd1
Antonio17-ru <AntonDemeshkin1@yandex.ru>
2022-05-12 19:50:15 +0300

    Fix commit

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -1,25 +1,27 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import web.service.CarService;
 import web.service.CarServiceImpl;
 
 @Controller
 @RequestMapping("/cars")
 public class CarsController {
 
-    private CarServiceImpl carServiceImpl;
-
-    public CarsController(CarServiceImpl carServiceImpl) {
-        this.carServiceImpl = carServiceImpl;
+    private final CarService carService;
+    @Autowired
+    public CarsController(CarService carService) {
+        this.carService = carService;
     }
 
     @GetMapping()
     public String modelCar(@RequestParam(value = "count", defaultValue = "5") int num, Model model) {
-        model.addAttribute("carsModel", carServiceImpl.index(num));
+        model.addAttribute("carsModel", carService.index(num));
         return "cars";
     }
 
--- src/main/java/web/service/CarService.java
+++ src/main/java/web/service/CarService.java
@@ -1,9 +1,10 @@
 package web.service;
 
+import org.springframework.stereotype.Service;
 import web.models.ModelsCar;
 
 import java.util.List;
-
+@Service
 public interface CarService {
     List<ModelsCar> index(int num);
 }
